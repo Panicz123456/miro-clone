71eab4e040fe9ca87074020a00f1263ec5666ae2
patryk <paniczgg@gmail.com>
2024-12-11 12:02:15 +0100

    first board is in correct url

--- app/boards/[boardId]/_components/info.tsx
+++ app/boards/[boardId]/_components/info.tsx
@@ -1,5 +1,3 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
 export const Info = () => {
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -8,33 +8,13 @@ import { createRoomContext } from "@liveblocks/react";
 import type { Layer, Color } from "@/types/canvas";
 
 const client = createClient({
-  publicApiKey: "pk_prod_i9Hr54ihq8j8RKUmw2QrgUT55OcfSnnkICdtF6jbEWluRbdZ5AUe_CBZy5mmGaJG", // Ensure to replace this with your actual public API key
-  throttle: 16,
-  async resolveUsers({ userIds }) {
-    // Example: Fetch user details by IDs from your backend
-    return userIds.map((userId) => ({
-      name: `User ${userId}`,
-      avatar: `https://example.com/avatar/${userId}.png`,
-    }));
-  },
-  async resolveMentionSuggestions({ text, roomId }) {
-    // Example: Return user IDs matching the text for mentions
-    const userIds = ["john@example.com", "joanna@example.com", "jane@example.com"];
-    if (!text) {
-      return userIds;
-    }
-    return userIds.filter((userId) => userId.toLowerCase().includes(text.toLowerCase()));
-  },
+  publicApiKey: "pk_dev_6OLkqxJWieh67sRtgIxs-gJRN-kZfvBhxLYtG-jku6l0vS-UJNj-SPCuVlIs-eaV", // Ensure to replace this with your actual public API key
 });
 
 // Presence represents the properties that exist on every user in the Room
 // and that will automatically be kept in sync. Accessible through the
 // `user.presence` property. Must be JSON-serializable.
 type Presence = {
-  cursor: { x: number; y: number } | null;
-  selection: string[];
-  pencilDraft: [x: number, y: number, pressure: number][] | null;
-  pencilColor: Color | null;
 };
 
 // Optionally, Storage represents the shared document that persists in the
@@ -42,8 +22,6 @@ type Presence = {
 // LiveList, LiveMap, LiveObject instances, for which updates are
 // automatically persisted and synced to all connected clients.
 type Storage = {
-  layers: LiveMap<string, LiveObject<Layer>>;
-  layerIds: LiveList<string>;
 };
 
 // Optionally, UserMeta represents static/readonly metadata on each user, as
