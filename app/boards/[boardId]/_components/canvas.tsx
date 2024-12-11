46b0a5e5f641d4fbe54e1cca0317acb71aa21765
patryk <paniczgg@gmail.com>
2024-12-11 16:58:53 +0100

    dodalem Canvas state do przelaczania pomiedziy trybami

--- app/boards/[boardId]/_components/canvas.tsx
+++ app/boards/[boardId]/_components/canvas.tsx
@@ -1,24 +1,39 @@
 "use client";
 
+import { useState } from "react";
+
+import { CanvasState, CanvasMode } from "@/types/canvas";
+
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-
-import { useSelf } from "@/liveblocks.config";
+import { useHistory, useCanRedo, useCanUndo } from "@/liveblocks.config";
 
 interface iAppProps {
   boardId: string;
 }
 
 export const Canvas = ({ boardId }: iAppProps) => {
-  const info = useSelf((me) => me.info);
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
 
-  console.log(info)
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
+
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   );
 };
